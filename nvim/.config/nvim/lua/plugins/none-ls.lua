a718dc9008cdf75deffe4515f2148c4e5f753b15
Florian Wolff <florian.wolff@vom-hellweg.de>
2025-04-25 10:16:11 +0200

    feat( nvim/.config/nvim/lua/plugins/none-ls.lua): added support for nix

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,6 +6,9 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.code_actions.statix,
+        null_ls.builtins.diagnostics.statix,
+        null_ls.builtins.formatting.nixfmt,
 				null_ls.builtins.formatting.gofmt,
 				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.formatting.golines,

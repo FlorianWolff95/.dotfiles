b0c5f93e8abac99656f8a78720ccea60fcf190b8
Florian Wolff <florian.wolff@purelei.com>
2025-07-29 13:08:30 +0000

    feat(nvim/lua/plugins): update configs for treesitter and telescope

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,44 +5,26 @@ return {
     lazy = false,
     branch = "main",
     config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup {
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "css",
-          "dockerfile",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "python",
-          "rust",
-          "typescript",
-          "vim",
-        },
-        ignore_install = { "go" }, -- golang parser is not working properly 2025-05-28
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = true,
-        },
-        indent = {
-          enable = true,
-        },
-        textobjects = {
-          move = {
-            enable = true,
-            goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-            goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-            goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-            goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-          },
-        },
+      require('nvim-treesitter').setup()
+      local ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "css",
+        "go",
+        "dockerfile",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "rust",
+        "typescript",
+        "vim",
       }
+
+      require('nvim-treesitter').install(ensure_installed)
     end
   }
 }

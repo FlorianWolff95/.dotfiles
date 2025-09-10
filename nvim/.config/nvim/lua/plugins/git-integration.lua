ee6b04414d810a6996535cd927e0cd7b4a985b5d
FlorianPurelei <florian.wolff@purelei.com>
2025-09-10 09:29:31 +0200

    feat(nvim): update gitsigns

--- nvim/.config/nvim/lua/plugins/git-integration.lua
+++ nvim/.config/nvim/lua/plugins/git-integration.lua
@@ -1,14 +1,20 @@
 return {
-	{
-		"tpope/vim-fugitive",
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
+  { "tpope/vim-fugitive" },
 
-			vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-		end,
-	},
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local map = function(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+          end
+
+          map("n", "<leader>gh", gs.preview_hunk, "Preview hunk")
+          map("n", "<leader>gt", gs.toggle_current_line_blame, "Toggle line blame")
+        end,
+      })
+    end,
+  },
 }
